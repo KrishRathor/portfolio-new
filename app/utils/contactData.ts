315df123e2b588c63744d5bd61cda48b96579ab8
Krish Rathor <krishrathor18@gmail.com>
2024-06-13 15:44:00 +0530

    portfolio

---
+++ app/utils/contactData.ts
@@ -0,0 +1,8 @@
+import { contactDataType } from "../types";
+
+export const contactData: contactDataType = {
+  email: "krishrathor18@gmail.com",
+  github: "https://github.com/KrishRathor",
+  linkedIn: "https://www.linkedin.com",
+  x: "https://x.com/KrishRathor18",
+};
